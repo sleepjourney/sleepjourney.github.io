6e3c068dd19b32901c4771dcd1be8bec03c24dca
Emily Zhao <ezzhao@berkeley.edu>
2021-12-01 13:52:35 -0800

    Second commit

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
 
-  $('#navbar').css('opacity', '0');
-  $('#navbar').css('top', '-5.5vh');
-  $('#content').css('opacity', '0');
+  $("#navbar").css("opacity", "0");
+  $("#navbar").css("top", "-5.5vh");
+  $("#content").css("opacity", "0");
 
   $(window).scroll(function() {
 
     var scroll = $(window).scrollTop();
 
-    var object = $('#content');
+    var object = $("#content");
 
     var position = object.offset().top;
 
     if (scroll > position) {
-      $('#navbar').css('top', '0');
-      $('#navbar').css('opacity', '0.8');
-      $('#content').css('animation-name', 'fade-in-bottom');
+      $("#navbar").css("top", "0");
+      $("#navbar").css("opacity", "0.8");
+      $("#content").css("animation-name", "fade-in-bottom");
     } else {
-      $('#navbar').css('top', '-5.5vh');
-      $('#navbar').css('opacity', '0');
+      $("#navbar").css("top", "-5.5vh");
+      $("#navbar").css("opacity", "0");
     }
 
   });
